7376cea398239120bb3966cc83fd915fb15e9bc0
HuyTran1996 <tqhuy1996.developer@gmail.com>
2021-02-18 23:56:04 +0700

    config queue

--- provider/rabbitmq_provider.py
+++ provider/rabbitmq_provider.py
@@ -1,11 +1,19 @@
 import pika
+import json
 
 
 def connection_rabbitmq(config):
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=config.get('RABBITMQ', 'HOST')))
+        pika.ConnectionParameters(host=config.get('RABBITMQ', 'HOST'),
+                                  virtual_host=config.get('RABBITMQ', 'VIRTUAL_HOST'),
+                                  port=config.get('RABBITMQ', 'PORT')))
     channel = connection.channel()
 
-    channel.queue_declare(queue='task_queue', durable=True)
+    with open('queue_config.json') as json_config_file:
+        data = json.load(json_config_file)
+        for item in data["Message"]:
+            if item["Active"]:
+                channel.queue_declare(queue=item["QueueName"], durable=True)
+
     print("Connect RabbitMQ")
     return channel
